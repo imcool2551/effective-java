d9b11bd861b1baeb94588c4e5523981f22dafdad
SangWook <imcool2551@gmail.com>
2022-02-15 15:16:52 +0900

    item 12

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
-import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Object> list = new ArrayList<>();
+        System.out.println("list = " + list);
     }
 }
