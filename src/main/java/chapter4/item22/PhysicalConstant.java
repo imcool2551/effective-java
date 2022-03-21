8eb6912beeab32cf776737834475a6e0aba9fc88
SangWook <imcool2551@gmail.com>
2022-03-21 19:24:06 +0900

    item 22

---
+++ src/main/java/chapter4/item22/PhysicalConstant.java
@@ -0,0 +1,13 @@
+package chapter4.item22;
+
+// 코드 22-2 상수 유틸리티 클래스 (140쪽)
+public class PhysicalConstant {
+    private PhysicalConstant() { }  // 인스턴스화 방지
+
+    public static final double AVOGADROS_NUMBER = 6.022_140_857e23;
+
+    public static final double BOLTZMANN_CONST  = 1.380_648_52e-23;
+
+    public static final double ELECTRON_MASS    = 9.109_383_56e-31;
+}
+
---
+++ src/main/java/chapter4/item22/PhysicalConstants.java
@@ -0,0 +1,11 @@
+package chapter4.item22;
+
+// 코드 22-1 상수 인터페이스 안티패턴 - 사용금지! (139쪽)
+public interface PhysicalConstants {
+    double AVOGADROS_NUMBER   = 6.022_140_857e23;
+
+    double BOLTZMANN_CONSTANT = 1.380_648_52e-23;
+
+    double ELECTRON_MASS      = 9.109_383_56e-31;
+}
+
