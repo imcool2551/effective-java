d026670f395839f8a3c93341e86f6ae7bf80e61f
SangWook <imcool2551@gmail.com>
2022-03-28 11:54:49 +0900

    item 28

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,19 @@
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Object> list = new ArrayList<>();
-        System.out.println("list = " + list);
+        Object[] objArray = new Long[1];
+    }
+
+    static int numElementsInCommon(Set<?> s1, Set<?> s2) {
+        int result = 0;
+        for (Object o1 : s1) {
+            if (s2.contains(o1)) {
+                result++;
+            }
+        }
+        return result;
     }
 }
